387d0adc249c68d22606f9beb2b8f79f751f2885
ncoca28 <ncoca28@gmail.com>
2023-03-22 14:29:50 +0100

    Js

--- js/app.js
+++ js/app.js
@@ -2,5 +2,3 @@ import './flip-element.js';
 import './social-network.js';
 import './swipeable-element.js';
 import './modal.js';
-
-console.log("hola");
--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,6 @@
 export default (() => {
     
+    let buyButton = document.querySelector(".header-checkout-button");
+    console.log(buyButton);
   
 })();
